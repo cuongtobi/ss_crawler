2359583f797cb180e36d8af3a4680cc3255a9220
cuongtobi <cuongtobi29@gmail.com>
2022-04-25 15:57:35 +0700

    :hammer: Implement get page title function

--- examples/index.php
+++ examples/index.php
@@ -3,6 +3,11 @@ require '../src/SSCrawler.php';
 
 use SS\Crawler as Crawler;
 
-$crawler = new Crawler;
+$crawler = new Crawler(
+    'https://cuongtobi.github.io/scraping-don-gian-trong-php-bang-curl.html',
+    ['User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'],
+);
 
-var_dump($crawler);
+echo "<pre>";
+var_dump($crawler->getPageTitle());
+echo "</pre>";
